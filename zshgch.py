46d2edda38c247cd072f703b730006409c590a94
spiritmans <liush20150721@163.com>
2015-11-24 15:38:31 +0800

    remove and modified

--- zshgch.py
+++ zshgch.py
@@ -7,35 +7,34 @@ password = '1234'
 num = 0
 
 while True:
-   name = raw_input('Please input your name:').strip()
+    name = raw_input('\033[34mPlease input your name:\033[0m').strip()
     if name  == user:
         while True:
             if num < 3:
-                pw = raw_input('Please input your password:')
+                pw = raw_input('\033[34mPlease input your password:\033[0m')
                 if pw != password:
-                    print "Wrong input,try again!"
+                    print "\033[31mWrong input,try again!\033[0m"
                     num += 1
                     continue
                 else:
-                    print "Welcome to login.  %s"%name
-                    print '''There are six mode for you:
-1.search
-2.add
-3.delete
-4.modify
-5.insert
-0.exit'''
+                    print "\033[32mWelcome to login. %s\033[0m"%name
                 break
             else:
-                print "Too many wrong input,exit!!"
+                print "\033[31mToo many wrong input,exit!!\033[0m"
                 sys.exit()
     else:
-        print "There is no %s"%name
+        print "\033[31mThere is no %s\033[0m"%name
         continue
     break
 ######################################
 while True:
-    select = raw_input('Please input your choice:1/2/3/4/5/0/ ').strip()
+    print '''There are 5 mode for you:
+    1.search
+    2.add
+    3.delete
+    4.modify
+    0.exit'''
+    select = raw_input('\033[33mPlease input your choice:1/2/3/4/0/ \033[0m').strip()
     if len(select) == 0:continue
     if select == '0':
         sys.exit()
@@ -43,107 +42,126 @@ while True:
         while True:
             match = 0
             info = file('info.txt')
-            sch = raw_input("Please input sth that you want to search(The exit/quit for exit):")
+            sch = raw_input("\033[32mPlease input sth that you want to search(The exit/quit for return):\033[0m")
             if sch == 'exit' or sch == 'quit':break
+            if len(sch) == 0:continue
             while True:
                 line = info.readline()
                 if len(line) == 0:break
                 if sch in line:
                     print line,
-                    info.close()
                     match = 1
                 else:
                     pass
             if match == 0:
-                print "There is no informations for %s" %sch
+                print "\033[31mThere is no informations for %s\033[0m" %sch
     elif select == '2':
         while True:
-            print '''Add format:
-            num     name    dep     tel'''
-            add = raw_input("Please input sth that you want to add(The exit/quit for exit):")
+            print '''Add format:num     name    dep     tel'''
+            add = raw_input("\033[32mPlease input sth that you want to add(The exit/quit for return):\033[0m")
             if len(add) == 0:continue
             if add == 'exit' or add == 'quit':break
-            info == file('info.txt','a')
+            info = file('info.txt','a')
             info.write('%s\n' %(add))
             info.close()
     elif select == '3':
         while True:
             os.system('cat info.txt')
-            delete = raw_input('Please input the name whom you want to delete(The exit/quit for exit):').strip()
-            if delete == 'exit' or delete == 'quit':break
-            if len(delete) == 0:continue
+            name = raw_input('\033[32mPlease input name whom you want to delete(The exit/quit for exit):\033[0m').strip()
+            if name == 'exit' or name == 'quit':break
+            if len(name) == 0:continue
             info = file('info.txt')
             list = info.readlines()
-            if 
-                new = ""
-                for line in list:
-                    if delete not in line:
-                        new = new + line
-                info.close()
-                info = file('info.txt','w')
-                info.write(new)
-                info.close()
-            else:
-                print "There is no %s"%delete
+            new = ""
+            for line in list:
+                x = line.split()
+                if name == x[1]:
+                    print x
+                    pass
+                else:
+                    print "\033[31mWrong name,try aging!\033[0m"
+                    new = new + line
+            info.close()
+            info = file('info.txt','w')
+            info.write(new)
+            info.close()
     elif select == '4':
         while True:
             os.system('cat info.txt')
-            name = raw_input("Please input the name whom you want to change(The exit/quit for exit):").strip()
-            if len(name) == 0:continue
-            fi name == 'exit' or name == 'quit':break
-            mod = raw_input("Please input your choice:num/name/dep/tel/exit/").strip()
             info = file('info.txt')
             list = info.readlines()
-            if mod =='exit':break
-            if len(mod) == 0:continue
-            if mod == 'num':
-                num = raw_input("Please input your num:")
-                new = ""
-                for line in list:
-                    x = line.split()
-                    if name == x[1]:
-                        x[0] = num
-                        new = new + '\t'.join(x) + '\n'
-                    else:
-                        new = new + line
-                print new
-            info.close()
-            info = file('info.txt','w')
-            info.write(new)
-            info.close()
-
-
-            elif option == 'name':
-            elif option == 'dep':
-            elif option == 'tel':
-            print new
-            info.close()
-            info = file('info.txt','w')
-            info.write(new)
-            info.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
-#    elif select == '5':
-        
-    
-    
+            name = raw_input("\033[32mPlease input the name whom you want to change(The exit/quit for exit):\033[0m").strip()
+            if len(name) == 0:continue
+            if name == 'exit' or name == 'quit':break
+            m = 0
+            for line in list:
+                if name in line:
+                    print line,
+                    m = 1
+                    while True:
+                        mod = raw_input("\033[33mPlease input your choice:num/name/dep/tel/exit/\033[0m ").strip()
+    #                    info = file('info.txt')
+    #                    list = info.readlines()
+                        if mod =='exit':break
+                        if len(mod) == 0:continue
+                        if mod == 'num':
+                            num = raw_input("\033[34mPlease input your number:\033[0m")
+                            new = ""
+                            for line in list:
+                                x = line.split()
+                                if name == x[1]:
+                                    x[0] = num
+                                    new = new + '\t'.join(x) + '\n'
+                                else:
+                                    new = new + line
+                            print new
+                            info.close()
+                            info = file('info.txt','w')
+                            info.write(new)
+                            info.close()
+                        elif mod == 'name':
+                            name_ch = raw_input("\033[34mPlease input your name:\033[0m").strip()
+                            new = ""
+                            for line in list:
+                                x = line.split()
+                                if name == x[1]:
+                                    x[1] = name_ch
+                                    new = new + '\t'.join(x) + '\n'
+                                else:
+                                    new = new + line
+                            print new
+                            info.close()
+                            info = file('info.txt','w')
+                            info.write(new)
+                            info.close()
+                        elif mod == 'dep':
+                            dep = raw_input("\033[34mPlease input your department:\033[0m")
+                            new = ""
+                            for line in list:
+                                x = line.split()
+                                if name == x[1]:
+                                    x[2] = dep
+                                    new = new + '\t'.join(x) + '\n'
+                                else:
+                                    new = new + line
+                            print new
+                            info.close()
+                            info = file('info.txt','w')
+                            info.write(new)
+                            info.close()
+                        elif mod == 'tel':
+                            tel = raw_input("\033[34mPlease input your telnumber:\033[0m")
+                            new = ""
+                            for line in list:
+                                x = line.split()
+                                if name == x[1]:
+                                    x[3] = tel
+                                    new = new + '\t'.join(x) + '\n'
+                                else:
+                                    new = new + line
+                            print new
+                            info.close()
+                            info = file('info.txt','w')
+                            info.write(new)
+                            info.close()
+            if m == 0:print "\033[31mThere is no %s\033[0m"%name
