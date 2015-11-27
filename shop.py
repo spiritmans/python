f4431f90074ea7a01b10dc40d138107fcb7850b7
spiritmans <liush20150721@163.com>
2015-11-27 15:46:26 +0800

    modified shop.py

--- shop.py
+++ shop.py
@@ -16,12 +16,13 @@ while True:
 cart_list = []
 while True:
     cost_list = []
+    #打印商品列表
     for a in product:
         p = a
         c = cost[product.index(a)]
         cost_list.append(c)
      	print p, c
-
+    #筛选可以购买和不能购买列表
     no_list = []
     store_list = []
     for b in cost_list:
@@ -31,14 +32,16 @@ while True:
         else:
             x = product[cost.index(b)]
             no_list.append(x)
-    if len(store_list) == 0:
+    if len(store_list) == 0:		#当可购买列表为0时，退出
         print 'You have no enough money to buy %s'%no_list
         print 'You can not to buy anything,exit!'
         sys.exit()
-    elif len(no_list) != 0:
+    elif len(no_list) != 0:		#当不可购买列表不为0时，打印不可购买列表
         print 'You have no enough money to buy %s'%no_list
         pass
+    #打印可购买列表
     print 'You can choose the product that you want to buy from %s'%store_list
+    #开始选择购买产品
     choice = raw_input('Please input your choice:').strip()
     if choice == 'exit':break
     if choice in store_list:
